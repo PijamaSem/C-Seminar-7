f874f176db47302d4c258304a7c9961ea502333f
Pijama_sem <example@example.com>
2022-11-19 23:11:44 +0300

    task 2

--- Program.cs
+++ Program.cs
@@ -38,4 +38,38 @@ double RandomDouble(int minVal, int maxVal, int round)
             double k= new Random().NextDouble();
             return Math.Round(m+k,1);
     }
-*/   
+*/
+
+//Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+//и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+/*
+int [,] CreateArrya = {{1,2,34,345,345},{4,6,345,345,32},{42,61,34,34,3}}; // Массив задан статически, разобраться как это делается 
+ShowArray(CreateArrya);
+Console.Write("Input number of rows, start from (0): ");
+int row  = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number of colums, start from (0): ");
+int colums  = Convert.ToInt32(Console.ReadLine());
+FindNum(CreateArrya,row,colums);
+
+void FindNum (int[,] array, int row, int colums)
+    {
+        if( row <= array.GetLength(0) &&  colums <= array.GetLength(1) ){
+        int num = array[row,colums];
+        Console.WriteLine($"You number is {array[row,colums]}");
+        }else{
+         Console.WriteLine("Out of range");
+        }       
+    }
+void ShowArray (int[,] array)
+    {
+    for(int i=0; i<array.GetLength(0); i++)
+        {
+            for(int j=0; j<array.GetLength(1); j++)
+                {
+                    Console.Write(array[i,j] + " ");
+                }
+            Console.WriteLine(" ");
+        }
+    }
+*/
